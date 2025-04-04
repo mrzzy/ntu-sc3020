89a1796926cddcfa92d234435a0f49f6b739ff58
Zhu Zhanyan <program.nom@gmail.com>
2025-04-04 11:01:49 +0800

    test(project2): add test to generate qep for all 22 tpc-h queries

---
+++ project2/test_preprocessing.py
@@ -0,0 +1,18 @@
+#
+# SC3020
+# Project 2
+# Preprocessing Unit Tests
+#
+
+import os
+import json
+from pathlib import Path
+
+from preprocessing import Postgres
+
+
+def test_postgres_explain(db: Postgres):
+    # run explain on each query in queries
+    query_dir = Path(__file__).parent / "queries"
+    for query_file in query_dir.glob("*.sql"):
+        db.explain(query_file.read_text())
