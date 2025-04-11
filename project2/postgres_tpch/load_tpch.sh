936c516ed5faf804b02b3db872612a54098cb1cc
Zhu Zhanyan <program.nom@gmail.com>
2025-04-11 22:34:46 +0800

    infra(project2): bundle tpch data into postgres_tpch container

--- project2/postgres_tpch/load_tpch.sh
+++ project2/postgres_tpch/load_tpch.sh
@@ -6,19 +6,17 @@
 
 set -ueo pipefail
 
-# download TPC-H data from huggingface
+# link data files to /tmp/dss-data
 # pg_tpch expects data at /tmp/dss-data
-echo "pg_tpch: downloading data"
+echo "pg_tpch: linking data"
 mkdir -p /tmp/dss-data
 for file in customer.csv  lineitem.csv  nation.csv  orders.csv  part.csv  partsupp.csv  region.csv  supplier.csv
 do 
-  curl -LO --output-dir /tmp/dss-data  https://huggingface.co/datasets/mrzzy/tpc-h/resolve/main/$file
+  ln -sf /data/$file /tmp/dss-data/$file 
 done
 
 # use pg_tpch sql scripts to load TPC-H data
 echo "pg_tpch: loading data"
-ls -l /pg_tpch/tpch-load.sql
-cat /pg_tpch/tpch-load.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /pg_tpch/tpch-load.sql
 
 echo "pg_tpch: creating primary keys"
@@ -29,7 +27,3 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /p
 
 echo "pg_tpch: creating indexes"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /pg_tpch/tpch-index.sql
-
-# cleanup data files no longer needed
-rm -rf /tmp/dss-data
-
